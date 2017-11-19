14cf3b8d61bade79093c114c0f1cf2d851b86c35
Xiaollu Hong <xiaoluhong@rancher.com>
2017-11-19 18:47:43 +0800

    no message

--- docs/jekyll.sh
+++ docs/jekyll.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+docker run --rm -ti -p 4000:4000 -v  $PWD:/home/site  registry.cn-shenzhen.aliyuncs.com/rancher_cn/rancher-docs:base jekyll  clean
 docker run --rm -ti -p 4000:4000 -v  $PWD:/home/site  registry.cn-shenzhen.aliyuncs.com/rancher_cn/rancher-docs:base jekyll s w -H 0.0.0.0
